a00cd263a8dbc4b48da4cf8f31c5db99fb5feff2
Tatibnomb <49379010@est.ort.edu.ar>
2025-10-24 12:10:29 -0300

    .

--- Assets/DeteccionDeColisiones.cs
+++ Assets/DeteccionDeColisiones.cs
@@ -15,12 +15,18 @@ public class DeteccionDeColisiones : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && hintPanel != null)
+        {
+            Debug.Log("Jugador detectado: activando panel");
             hintPanel.SetActive(true);
+        }
     }
 
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player") && hintPanel != null)
+        {
+            Debug.Log("Jugador salió del área: desactivando panel");
             hintPanel.SetActive(false);
+        }
     }
 }
