0002892c169db2cd9d27fd8b1a0944a59da80a3c
Tatibnomb <49379010@est.ort.edu.ar>
2025-12-05 09:24:40 -0300

    memotest cargado

---
+++ Assets/CARD.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CARD : MonoBehaviour
+{
+    public int cardID;
+
+    [Header("Materiales")]
+    public Material materialFrente;
+    public Material materialDorso;
+
+    private MeshRenderer rend;
+    private bool cartaMostrada = false;
+
+    private XRSimpleInteractable interactable;
+
+    private void Awake()
+    {
+        rend = GetComponentInChildren<MeshRenderer>();
+        interactable = GetComponent<XRSimpleInteractable>();
+    }
+
+    private void Start()
+    {
+        rend.sharedMaterial = materialDorso;
+        interactable.selectEntered.AddListener(OnCardSelected);
+    }
+
+    private void OnCardSelected(SelectEnterEventArgs args)
+    {
+        if (!cartaMostrada)
+        {
+            Mostrar();
+            GameManager.Instance.CartaSeleccionada(this);
+        }
+    }
+
+    public void Mostrar()
+    {
+        rend.sharedMaterial = materialFrente;
+        cartaMostrada = true;
+    }
+
+    public void Ocultar()
+    {
+        rend.sharedMaterial = materialDorso;
+        cartaMostrada = false;
+    }
+
+    private void OnDestroy()
+    {
+        interactable.selectEntered.RemoveListener(OnCardSelected);
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private CARD carta1 = null;
+    private CARD carta2 = null;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void CartaSeleccionada(CARD carta)
+    {
+        if (carta1 == null)
+        {
+            carta1 = carta;
+        }
+        else if (carta2 == null && carta != carta1)
+        {
+            carta2 = carta;
+            StartCoroutine(VerificarMatch());
+        }
+    }
+
+    private IEnumerator VerificarMatch()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        if (carta1.cardID == carta2.cardID)
+        {
+            // Animación de unión
+            yield return StartCoroutine(AnimarMatch(carta1, carta2));
+
+            // Esperar 3 segundos antes de destruir
+            yield return new WaitForSeconds(3f);
+
+            // Destruir las cartas
+            Destroy(carta1.gameObject);
+            Destroy(carta2.gameObject);
+        }
+        else
+        {
+            carta1.Ocultar();
+            carta2.Ocultar();
+        }
+
+        carta1 = null;
+        carta2 = null;
+    }
+
+    private IEnumerator AnimarMatch(CARD c1, CARD c2)
+    {
+        Vector3 posFinal = (c1.transform.position + c2.transform.position) / 2f;
+
+        float duracion = 0.3f;
+        float tiempo = 0f;
+
+        Vector3 start1 = c1.transform.position;
+        Vector3 start2 = c2.transform.position;
+
+        while (tiempo < duracion)
+        {
+            tiempo += Time.deltaTime;
+            float t = tiempo / duracion;
+
+            c1.transform.position = Vector3.Lerp(start1, posFinal, t);
+            c2.transform.position = Vector3.Lerp(start2, posFinal, t);
+
+            yield return null;
+        }
+
+        float separacionMinima = 0.3f;
+
+        c1.transform.position = posFinal + new Vector3(0, -separacionMinima / 2f, 0);
+        c2.transform.position = posFinal + new Vector3(0, separacionMinima / 2f, 0);
+    }
+}
